174cb8af4a119ded967be25f89e0b40d6275c9a8
mikilanjlo <mikilanjlo@yandex.by>
2019-02-24 13:58:59 +0300

    create

---
+++ Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadawansTask13
+{
+    class Employee
+    {
+        private string surname;
+        private int age;
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string surname, int age)
+        {
+            this.surname = surname;
+            this.age = age;
+        }
+
+        private string GetAge()
+        {
+            return "" + age;
+        }
+
+        public void SetSurname(string surname)
+        {
+            this.surname = surname;
+        }
+
+        public string GetInfo()
+        {
+            return $"Surname: {this.surname}, Age: {this.age}";
+        }
+    }
+}
